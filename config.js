0aa021fbd1ff6cd9e5e958036a334f858447a54d
Aditya Yadav <166515021+aditya0yadav@users.noreply.github.com>
2024-11-07 01:20:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize('u411184336_acutusaidb', 'u411184336_acutusaidb'
   port: 3306,        
   dialect: 'mysql',
   pool: {
-    max: 50,         
+    max: 500,         
     min: 25,         
     acquire: 30000,  
     idle: 10000     
